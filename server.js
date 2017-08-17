7d71a639005b282ffafe518746140394c00a7de3
mkolenski <kolenskim@gmail.com>
2017-08-17 16:37:15 -0400

    Projects Page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
    });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+     pageTitle: 'Projects'
+   });
+});
+
 // /bad
 // json with error msg property
 
